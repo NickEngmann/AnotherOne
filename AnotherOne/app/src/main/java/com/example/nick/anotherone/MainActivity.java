f4ec0f632ca0b304b38949b15226d15bbcf013b5
NickySlicksHaha <nickengmann_2@yahoo.com>
2016-06-02 19:24:47 -0400

    Gifs playing
    Got Gifs to play, they are still small

--- AnotherOne/app/src/main/java/com/example/nick/anotherone/MainActivity.java
+++ AnotherOne/app/src/main/java/com/example/nick/anotherone/MainActivity.java
@@ -8,13 +8,23 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textViewInfo;
+    GifView gifView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gifView = (GifView) findViewById(R.id.gif_view);
+        textViewInfo = (TextView) findViewById(R.id.textinfo);
+
+        String stringInfo = "";
+        stringInfo += "Duration: " + gifView.getMovieDuration() + "\n";
+        stringInfo += "W x H: " + gifView.getMovieWidth() + " x " + gifView.getMovieHeight() + "\n";
+
+        textViewInfo.setText(stringInfo);
     }
 
 }
